40fd63d15616569fa733f197911ebdeaa90f5631
Reshid-505 <hreshid2016@gmail.com>
2026-02-01 14:17:15 +0400

    Initial Commit

---
+++ RestaurantList/RestaurantListApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestaurantListApp.swift
+//  RestaurantList
+//
+//  Created by Reshid Huseynov on 01.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct RestaurantListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

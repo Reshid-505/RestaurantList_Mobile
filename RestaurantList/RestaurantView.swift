b4253008a7069a48eb680025d3ae7b96c376d0cf
Reshid-505 <hreshid2016@gmail.com>
2026-02-15 14:22:04 +0400

    feat: added restaurant favorites action

--- RestaurantList/RestaurantView.swift
+++ RestaurantList/RestaurantView.swift
@@ -8,14 +8,33 @@
 import SwiftUI
 
 struct RestaurantView: View {
+    @State private var showDialog: Bool = false
+    @State private var isFavorite: Bool = false
+    
     var restaurant: Restaurant
     var body: some View {
         VStack(alignment: .leading, spacing: 16){
-            Image(restaurant.imageName)
-                .resizable()
-//                .scaledToFill()
-                .frame(height: 200)
-                .cornerRadius(20)
+            ZStack(alignment: .top){
+                Image(restaurant.imageName)
+                    .resizable()
+                //                .scaledToFill()
+                    .frame(height: 200)
+                    .cornerRadius(20)
+                
+                HStack{
+                    Spacer()
+                    
+                    Image(systemName: isFavorite ? "heart.fill" : "heart")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 25)
+                        .foregroundStyle(isFavorite ? .red : .black)
+                        .onTapGesture {
+                            isFavorite.toggle()
+                        }
+                    
+                }
+            }
             VStack(alignment: .leading){
                 Text(restaurant.name)
                     .font(.title2)
@@ -25,7 +44,29 @@ struct RestaurantView: View {
                     .foregroundStyle(.gray)
                     .font(.footnote)
             }
-            .padding(.leading)
+            .padding([.leading, .bottom])
+        }
+        .onTapGesture {
+            showDialog = true
+        }
+        .confirmationDialog("What do you want?", isPresented: $showDialog, titleVisibility: .visible) {
+            Button{
+                isFavorite.toggle()
+            }label:{
+                Text(isFavorite ? "Remove from favorites" : "Add to favorites")
+            }
+            
+            Button{
+                
+            }label:{
+                Text("Copy link")
+            }
+            
+            Button{
+                
+            }label:{
+                Text("Open in map")
+            }
         }
         
 //        HStack{
